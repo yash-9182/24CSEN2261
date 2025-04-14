5f9f5aa3b584e351ccc81eb168945d82316056d1
yash-9182 <yeshu9182@gmail.com>
2025-04-14 18:38:20 +0530

    stack.py

---
+++ stack.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return None
+
+    def peek(self):
+        return self.stack[-1] if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+# Example Usage
+s = Stack()
+s.push(10)
+s.push(20)
+print(s.pop())  # Output: 20
